18d76cc46d4513467f5db0b794fb8f1e0b24d012
Andrei <andrei_brainiac@yahoo.com>
2021-04-18 02:57:18 +0300

    update main

--- src/main/java/com/residence/maintenance/MaintenanceApplication.java
+++ src/main/java/com/residence/maintenance/MaintenanceApplication.java
@@ -3,11 +3,12 @@ package com.residence.maintenance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Map;
+
 @SpringBootApplication
 public class MaintenanceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MaintenanceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MaintenanceApplication.class, args);
+    }
 }
