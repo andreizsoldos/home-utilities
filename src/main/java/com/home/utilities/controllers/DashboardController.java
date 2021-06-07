0d5e53d2fa5b81232c3ebc808a294a0842938126
Andrei <andrei_brainiac@yahoo.com>
2021-06-08 02:08:59 +0300

    #54 - refactor class name; add objects for every branch

---
+++ src/main/java/com/home/utilities/controllers/DashboardController.java
@@ -0,0 +1,27 @@
+package com.home.utilities.controllers;
+
+import com.home.utilities.configuration.userdetails.UserPrincipal;
+import com.home.utilities.entities.Branch;
+import com.home.utilities.service.IndexService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class DashboardController {
+
+    private final IndexService indexService;
+
+    @GetMapping("/user/dashboard")
+    public ModelAndView dashboardPage() {
+        final var mav = new ModelAndView("user-dashboard");
+        List.of(Branch.values()).forEach(b -> mav.addObject(
+              b.name().toLowerCase() + "LastModifiedDate",
+              indexService.getLastModifiedDate(b, UserPrincipal.getCurrentUser().getId())));
+        return mav;
+    }
+}
