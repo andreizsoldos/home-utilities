9c947b7fa497c71e3ad06c453adf35bac417300a
Andrei <andrei_brainiac@yahoo.com>
2021-06-08 13:33:18 +0300

    #73 - add method in ClientCodeService layer

--- src/main/java/com/home/utilities/service/ClientCodeService.java
+++ src/main/java/com/home/utilities/service/ClientCodeService.java
@@ -4,6 +4,7 @@ import com.home.utilities.entities.Branch;
 import com.home.utilities.entities.ClientCode;
 import com.home.utilities.payload.dto.ClientCodeDetails;
 import com.home.utilities.payload.request.ClientCodeRequest;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,4 +18,6 @@ public interface ClientCodeService {
     Optional<ClientCodeDetails> findFirstClientCode(List<Branch> branches, Long userId);
 
     Long getTotalClientCodes(Branch branch, Long userId);
+
+    Optional<String> getClientCodeNameWhoInsertedLastIndex(@Param("branch") Branch branch, @Param("userId") Long userId);
 }
