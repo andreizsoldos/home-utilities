fff95e2cd87dd58e69497ca90dae90484b0d66ea
Andrei <andrei_brainiac@yahoo.com>
2021-06-08 13:32:49 +0300

    #73 - add method in repository to get client name

--- src/main/java/com/home/utilities/repository/ClientCodeRepository.java
+++ src/main/java/com/home/utilities/repository/ClientCodeRepository.java
@@ -3,12 +3,12 @@ package com.home.utilities.repository;
 import com.home.utilities.entities.Branch;
 import com.home.utilities.entities.ClientCode;
 import com.home.utilities.payload.dto.ClientCodeDetails;
-import com.home.utilities.payload.dto.TotalClientCodes;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ClientCodeRepository extends JpaRepository<ClientCode, Long> {
 
@@ -29,4 +29,16 @@ public interface ClientCodeRepository extends JpaRepository<ClientCode, Long> {
           AND cc.branch = :branch
           """)
     Long getTotalNumberOfClientCodes(@Param("branch") Branch branch, @Param("userId") Long userId);
+
+    @Query("""
+          SELECT client.clientName as name FROM Index index
+          INNER JOIN index.clientCode client
+          WHERE index.id =
+              (SELECT max(i.id) from Index i
+              INNER JOIN i.clientCode cc
+              INNER JOIN cc.user u
+              ON u.id = :userId
+              AND cc.branch = :branch)
+          """)
+    Optional<String> getClientCodeNameWhoInsertedLastIndex(@Param("branch") Branch branch, @Param("userId") Long userId);
 }
