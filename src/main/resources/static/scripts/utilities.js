027fcdbbc86db2d3b3c59845e3c8dd2ee1037105
Andrei <andrei_brainiac@yahoo.com>
2021-06-12 01:04:44 +0300

    #58 - rename javascript file

--- src/main/resources/static/scripts/utilities.js
+++
@@ -1,9 +0,0 @@
-function focusInputBox() {
-  document.getElementById("newIndex").focus();
-}
-
-$(document).ready(function () {
-  $(window).on("load", function() {
-    $("#staticBackdrop").modal("show");
-  });
-});
