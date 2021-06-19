94b6283393b97e49b70ba3c9912b70242a91bc86
Andrei <andrei_brainiac@yahoo.com>
2021-06-20 02:05:25 +0300

    #38 - remove unnecessary files

--- src/main/resources/static/scripts/index.js
+++
@@ -1,9 +0,0 @@
-function focusInputBox() {
-  document.getElementById("newIndex").focus();
-}
-
-$(document).ready(function () {
-  $(window).on("load", function() {
-    $("#indexBackdrop").modal("show");
-  });
-});
