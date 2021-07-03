0dadf35042fa2ec23963c04b47610aee0d4f2fee
Andrei <andrei_brainiac@yahoo.com>
2021-07-03 23:51:40 +0300

    #68 - add custom exception

---
+++ src/main/java/com/home/utilities/exceptions/EmailException.java
@@ -0,0 +1,12 @@
+package com.home.utilities.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class EmailException extends RuntimeException {
+
+    public EmailException(final String message, final Object fieldValue) {
+        super(String.format("%s %s", message, fieldValue));
+    }
+}
