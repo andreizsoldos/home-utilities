7c6e3acd940b1eaf1a0dfd5e634af278f2c4a8ac
Andrei <andrei_brainiac@yahoo.com>
2021-07-10 21:00:15 +0300

    #100 - add thymeleaf builder interface

--- src/main/java/com/home/utilities/services/email/ThymeleafEmailBuilder.java
+++ src/main/java/com/home/utilities/services/email/ThymeleafEmailBuilder.java
@@ -1,8 +1,9 @@
 package com.home.utilities.services.email;
 
+import java.util.Locale;
 import java.util.Map;
 
 public interface ThymeleafEmailBuilder {
 
-    String build(String emailTemplate, Map<String, String> content);
+    String build(String emailTemplate, Map<String, String> content, Locale locale);
 }
