80f249bfd3b0d225a08932997587e40dd5a19d76
Andrei <andrei_brainiac@yahoo.com>
2021-07-30 21:21:56 +0300

    #107 - refactored javascript filename

---
+++ src/main/resources/static/scripts/progressbar.js
@@ -0,0 +1,27 @@
+$(window).on("load", function() {
+  var progressBar = document.getElementById("progressBar");
+  $(".check-icon").hide();
+  setTimeout(function () {
+    $(".check-icon").show();
+    smoothProgress(progressBar);
+  }, 1000);
+});
+
+function smoothProgress(e) {
+    var id = $("#" + e.id),
+    dur = parseFloat(id.attr("data-duration")) * 1000,
+    seq = 50,
+    max = parseInt(id.attr("max"), 10),
+    min = 0,
+    chunk = max / dur * seq;
+    id.val(max);
+    var location = document.getElementsByClassName("contBtn")[0].getAttribute("href");
+    var loop = setInterval(function() {
+        if(id.val() > min) {
+            id.val(id.val() - chunk);
+        } else {
+            clearInterval(loop);
+            window.location.href = location;
+        }
+    }, seq);
+}
