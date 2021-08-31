23d3b6e90077545ece2b1e704492034d27f43184
Andrei <andrei_brainiac@yahoo.com>
2021-08-31 14:24:53 +0300

    #122 - add property to hold old indexes

--- src/main/java/com/home/utilities/entities/Index.java
+++ src/main/java/com/home/utilities/entities/Index.java
@@ -9,6 +9,8 @@ import org.hibernate.envers.Audited;
 
 import javax.persistence.*;
 import javax.validation.constraints.PositiveOrZero;
+import java.util.ArrayList;
+import java.util.List;
 
 @Audited
 @Entity
@@ -22,6 +24,9 @@ public class Index extends UserAudit {
     @PositiveOrZero
     @Column(name = "value")
     private Double value;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "index")
+    private List<OldIndex> oldIndexes = new ArrayList<>();
 
     @ManyToOne(targetEntity = ClientCode.class, fetch = FetchType.LAZY, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
     @JoinColumn(name = "client_code_id", nullable = false)
