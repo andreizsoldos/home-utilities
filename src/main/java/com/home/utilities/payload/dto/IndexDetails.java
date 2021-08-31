de352ca9d05cd8249e67ecc1af7e9273ed4c0c0a
Andrei <andrei_brainiac@yahoo.com>
2021-08-31 14:26:30 +0300

    #122 - add methods to IndexDetails payload

--- src/main/java/com/home/utilities/payload/dto/IndexDetails.java
+++ src/main/java/com/home/utilities/payload/dto/IndexDetails.java
@@ -1,19 +1,55 @@
 package com.home.utilities.payload.dto;
 
+import com.home.utilities.services.util.DateTimeConverter;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.Instant;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class IndexDetails {
 
+    private static final String CHAR = "&nbsp"; // or use -
+
     private Long id;
     private Long clientId;
     private Double value;
     private Instant createdAt;
     private Instant modifiedAt;
+
+    public String getOldIndex(final List<OldIndexDetails> oldIndexesList) {
+        return oldIndexesList.stream()
+              .filter(i -> i.getIndexId().equals(this.id))
+              .map(i -> DateTimeConverter.toTime(i.getModifiedAt()) + " --> " + i.getValue())
+              .collect(Collectors.joining("<br>"));
+    }
+
+    public String insertTooltipTitle(final String leftTitle, final String rightTitle, final List<OldIndexDetails> oldIndexesList) {
+        final var leftChar = addDelimiter((8 - leftTitle.length()) / 2);
+        final var rightChar = addDelimiter((maxListLength(oldIndexesList) - 13 - rightTitle.length() - 1) < 0 ? 3 : maxListLength(oldIndexesList) - 13 - rightTitle.length() - 1);
+        final var middleChar = addDelimiter((maxListLength(oldIndexesList) - countDelimiter(leftChar) - leftTitle.length() - countDelimiter(rightChar)) < 0 ? 6 : maxListLength(oldIndexesList) - countDelimiter(leftChar) - leftTitle.length() - countDelimiter(rightChar));
+
+        return leftChar + leftTitle + middleChar + rightTitle + rightChar;
+    }
+
+    private Integer maxListLength(final List<OldIndexDetails> oldIndexesList) {
+        return oldIndexesList.stream()
+              .filter(i -> i.getIndexId().equals(this.id))
+              .map(i -> DateTimeConverter.toTime(i.getModifiedAt()) + " --> " + i.getValue())
+              .map(String::length)
+              .reduce(0, Integer::max);
+    }
+
+    private String addDelimiter(final Integer length) {
+        return CHAR.repeat(length);
+    }
+
+    private Integer countDelimiter(final String delimiter) {
+        return delimiter.replace(CHAR, " ").length();
+    }
 }
