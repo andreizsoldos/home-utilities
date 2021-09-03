ca122bd7ccdc92f1021fca4a74bba6cb086d2664
Andrei <andrei_brainiac@yahoo.com>
2021-09-03 20:22:00 +0300

    #60 - add OldIndexRequest payload class

---
+++ src/main/java/com/home/utilities/payload/request/OldIndexRequest.java
@@ -0,0 +1,31 @@
+package com.home.utilities.payload.request;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.home.utilities.validators.index.NewValueGreaterThanOldValue;
+import com.home.utilities.validators.index.ValidateNumeric;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+
+@Getter
+@Setter
+@NewValueGreaterThanOldValue(field = "value", fieldMatch = "lastIndex")
+public class OldIndexRequest {
+
+    @ValidateNumeric
+    @JsonDeserialize(using = IndexRequestDeserializer.class)
+    @NotNull(message = "{message.index.illegal.value}")
+    @PositiveOrZero(message = "{message.field.positive}")
+    private Double value;
+
+    private Double lastIndex;
+
+    public OldIndexRequest() {
+    }
+
+    public OldIndexRequest(final Double lastIndex) {
+        this.lastIndex = lastIndex;
+    }
+}
