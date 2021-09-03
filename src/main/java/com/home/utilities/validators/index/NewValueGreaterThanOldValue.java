51e77aec4833148f8f143cae6910fe528d36f4d6
Andrei <andrei_brainiac@yahoo.com>
2021-09-03 20:28:40 +0300

    #60 - add annotation for validating index values

---
+++ src/main/java/com/home/utilities/validators/index/NewValueGreaterThanOldValue.java
@@ -0,0 +1,21 @@
+package com.home.utilities.validators.index;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = ValidatorNewValueGreaterThanOldValue.class)
+@Target({ElementType.TYPE})
+@Retention(value = RetentionPolicy.RUNTIME)
+public @interface NewValueGreaterThanOldValue {
+    String message() default "{}";
+
+    String field();
+
+    String fieldMatch();
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
