4a8a11c93a36616c406e0a6f50bce48f99e75dc9
Andrei <andrei_brainiac@yahoo.com>
2021-09-03 20:21:46 +0300

    #60 - refactor NewIndexRequest payload class

--- src/main/java/com/home/utilities/payload/request/NewIndexRequest.java
+++ src/main/java/com/home/utilities/payload/request/NewIndexRequest.java
@@ -1,7 +1,10 @@
 package com.home.utilities.payload.request;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.home.utilities.validators.index.NewValueGreaterThanOldValue;
+import com.home.utilities.validators.index.OncePerDay;
+import com.home.utilities.validators.index.ValidateNumeric;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
@@ -9,10 +12,22 @@ import javax.validation.constraints.PositiveOrZero;
 
 @Getter
 @Setter
-@NoArgsConstructor
+@OncePerDay(field = "value", fieldMatch = "lastIndex")
+@NewValueGreaterThanOldValue(field = "value", fieldMatch = "lastIndex")
 public class NewIndexRequest {
 
-    @NotNull(message = "{message.field.empty}")
+    @ValidateNumeric
+    @JsonDeserialize(using = IndexRequestDeserializer.class)
+    @NotNull(message = "{message.index.illegal.value}")
     @PositiveOrZero(message = "{message.field.positive}")
-    private Double newValue;
+    private Double value;
+
+    private Double lastIndex;
+
+    public NewIndexRequest() {
+    }
+
+    public NewIndexRequest(final Double lastIndex) {
+        this.lastIndex = lastIndex;
+    }
 }
