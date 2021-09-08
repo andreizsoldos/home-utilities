cf444eef0ca6eb2659dc49eb160c7437505fd381
Andrei <andrei_brainiac@yahoo.com>
2021-09-08 14:00:11 +0300

    #139 - add method to find by id

--- src/main/java/com/home/utilities/services/ConfirmationTokenService.java
+++ src/main/java/com/home/utilities/services/ConfirmationTokenService.java
@@ -3,9 +3,13 @@ package com.home.utilities.services;
 import com.home.utilities.entities.ConfirmationToken;
 import com.home.utilities.entities.User;
 
+import java.util.Optional;
+
 public interface ConfirmationTokenService {
 
     ConfirmationToken findByToken(String token);
+
+    Optional<ConfirmationToken> findByUserId(Long userId);
 
     ConfirmationToken save(ConfirmationToken token);
 
