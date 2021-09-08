0a7c774b6801755e172e03ecd535157336b508f9
Andrei <andrei_brainiac@yahoo.com>
2021-09-08 14:01:15 +0300

    #139 - add SupportRequest payload

---
+++ src/main/java/com/home/utilities/payload/request/SupportRequest.java
@@ -0,0 +1,18 @@
+package com.home.utilities.payload.request;
+
+import com.home.utilities.validators.email.ValidateEmail;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class SupportRequest {
+
+    @ValidateEmail
+    @NotBlank(message = "{message.field.empty}")
+    private String email;
+}
