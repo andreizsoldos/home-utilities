452c02e3f4c80c02b9985811a27e5e3a45ba2f40
Andrei <andrei_brainiac@yahoo.com>
2021-09-08 14:00:37 +0300

    #139 - add method to find by userId

--- src/main/java/com/home/utilities/repository/ConfirmationTokenRepository.java
+++ src/main/java/com/home/utilities/repository/ConfirmationTokenRepository.java
@@ -3,7 +3,11 @@ package com.home.utilities.repository;
 import com.home.utilities.entities.ConfirmationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken, Long> {
 
     ConfirmationToken findByToken(String token);
+
+    Optional<ConfirmationToken> findByUserId(Long userId);
 }
