43fbeed5dd90790192ea431e679bbc0182c65426
Andrei <andrei_brainiac@yahoo.com>
2021-10-16 01:51:06 +0300

    #38 - test fonts

--- src/main/java/com/home/utilities/controllers/DashboardController.java
+++ src/main/java/com/home/utilities/controllers/DashboardController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -22,6 +24,16 @@ public class DashboardController {
     public ModelAndView dashboardPage() {
         final var mav = new ModelAndView("user-dashboard");
         final var userId = UserPrincipal.getCurrentUser().getId();
+
+        // Get all available font family names from GraphicsEnvironment
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        String[] familyNames = ge.getAvailableFontFamilyNames();
+
+        // Iterates familyNames array to display the available font's family names
+        for (String familyName : familyNames) {
+            System.out.println("Family name: " + familyName);
+        }
+
         List.of(Branch.values()).forEach(b -> {
             mav.addObject(b.name().toLowerCase() + "TotalClientCodes",
                   clientCodeService.getTotalClientCodes(b, userId));
@@ -33,6 +45,8 @@ public class DashboardController {
                   clientCodeService.getClientCodeNameWhoInsertedLastIndex(b, userId).orElse("-"));
             mav.addObject(b.name().toLowerCase() + "LastModificationDate",
                   indexService.getLastModificationDuration(b, userId));
+            mav.addObject(b.name().toLowerCase() + "FontFamily",
+                  new ArrayList<>(List.of(familyNames)));
         });
         return mav;
     }
