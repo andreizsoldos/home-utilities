c0cf089285e1c9cf1af0eb852939343a4d2eae33
Andrei <andrei_brainiac@yahoo.com>
2021-10-16 03:08:11 +0300

    #38 - test fonts on android

--- src/main/java/com/home/utilities/controllers/DashboardController.java
+++ src/main/java/com/home/utilities/controllers/DashboardController.java
@@ -29,11 +29,6 @@ public class DashboardController {
         GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
         String[] familyNames = ge.getAvailableFontFamilyNames();
 
-        // Iterates familyNames array to display the available font's family names
-        for (String familyName : familyNames) {
-            System.out.println("Family name: " + familyName);
-        }
-
         List.of(Branch.values()).forEach(b -> {
             mav.addObject(b.name().toLowerCase() + "TotalClientCodes",
                   clientCodeService.getTotalClientCodes(b, userId));
