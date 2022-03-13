881040f420886ea62e62aa80fd9f43703364ede8
Andrei <andrei_brainiac@yahoo.com>
2022-03-13 14:16:20 +0200

    #219 - refactor implementation for failed login attempts

--- src/main/java/com/home/utilities/configuration/security/authentication/AuthFailureHandler.java
+++ src/main/java/com/home/utilities/configuration/security/authentication/AuthFailureHandler.java
@@ -2,20 +2,25 @@ package com.home.utilities.configuration.security.authentication;
 
 import com.home.utilities.configuration.userdetails.service.BruteForceProtectionService;
 import com.home.utilities.entities.AccountStatus;
-import com.home.utilities.exceptions.FailedLoginException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.WebAttributes;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.web.servlet.LocaleResolver;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.ZoneId;
+import java.util.Date;
 
 public class AuthFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 
-    private static final String LOCKED = "AbstractUserDetailsAuthenticationProvider.locked";
+    private static final String LOCKED = "login.locked.message";
+    private static final String BAD_CREDENTIALS = "login.badCredentials.message";
+    private static final String LOCK_DURATION = "LOCK_DURATION";
     private static final String USERNAME = "username";
     private static final String DEFAULT_FAILURE_URL = "/login?error";
 
@@ -25,33 +30,44 @@ public class AuthFailureHandler extends SimpleUrlAuthenticationFailureHandler {
     @Autowired
     private MessageSource messageSource;
 
+    @Autowired
+    private LocaleResolver localeResolver;
+
     @Override
     public void onAuthenticationFailure(final HttpServletRequest request,
                                         final HttpServletResponse response,
-                                        AuthenticationException exception) throws IOException, ServletException {
+                                        final AuthenticationException exception) throws IOException, ServletException {
+
+        super.setDefaultFailureUrl(DEFAULT_FAILURE_URL);
+        super.onAuthenticationFailure(request, response, exception);
 
         final var email = request.getParameter(USERNAME);
         final var optionalUser = bruteForceService.findByEmail(email);
+        final var locale = localeResolver.resolveLocale(request);
+        final var maxFailedAttempts = bruteForceService.getMaximumFailedAttempts();
+
+        var errorMessage = messageSource.getMessage(BAD_CREDENTIALS, null, locale);
+        request.getSession().setAttribute(LOCK_DURATION, null);
 
         if (optionalUser.isPresent()) {
             final var user = optionalUser.get();
             if (user.getStatus().equals(AccountStatus.ACTIVE)) {
                 bruteForceService.registerFailedLoginAttempts(user);
-                final var maxFailedAttempts = bruteForceService.getMaximumFailedAttempts();
                 final var failedCounter = user.getFailedLoginAttempts();
                 if (maxFailedAttempts <= failedCounter) {
                     bruteForceService.lockAccount(user);
-                    exception = new FailedLoginException(messageSource.getMessage(
-                          LOCKED,
-                          null,
-                          request.getLocale()));
+                    errorMessage = messageSource.getMessage(LOCKED, new Object[]{maxFailedAttempts}, locale);
+                    request.getSession().setAttribute(LOCK_DURATION, Date.from(user.getEndLockTime().atZone(ZoneId.systemDefault()).toInstant()).getTime());
                 }
+            } else if (user.getStatus().equals(AccountStatus.LOCKED)) {
+                errorMessage = messageSource.getMessage(LOCKED, new Object[]{maxFailedAttempts}, locale);
+                request.getSession().setAttribute(LOCK_DURATION, Date.from(user.getEndLockTime().atZone(ZoneId.systemDefault()).toInstant()).getTime());
             }
         } else {
             // TODO - implement IP failure login here
         }
 
-        super.setDefaultFailureUrl(DEFAULT_FAILURE_URL);
-        super.onAuthenticationFailure(request, response, exception);
+        request.getSession()
+              .setAttribute(WebAttributes.AUTHENTICATION_EXCEPTION, errorMessage);
     }
 }
--- src/main/resources/static/scripts/login.js
+++ src/main/resources/static/scripts/login.js
@@ -1,4 +1,3 @@
-
 const event = document.createEvent('Event');
 event.initEvent('input', true, true);
 var x = document.getElementById("password");
@@ -30,5 +29,62 @@ function revealPassword() {
         y.title = "Show password";
         y.classList.remove("bi-eye-slash-fill");
         y.classList.add("bi-eye");
+    }
+}
+
+$(window).on("load", function() {
+    var exceptionErrorId = document.getElementById("exceptionError");
+    if (exceptionErrorId != null) {
+        var counterErrorId = document.getElementById("counterError");
+        var countDownDate = null;
+        if (counterErrorId != null) {
+            if (counterErrorId.getAttribute("data-counter") != null) {
+                countDownDate = counterErrorId.getAttribute("data-counter");
+            } else {
+                countDownDate = new Date().getTime();
+            }
+            countDownFailedLogin(counterErrorId, exceptionErrorId, countDownDate);
+        } else {
+            countDownDate = new Date().getTime();
+            countDownFailedLogin(counterErrorId, exceptionErrorId, countDownDate);
+        }
+    }
+})
+
+function countDownFailedLogin(counterErrorId, exceptionErrorId, countDownDate) {
+    if (countDownDate != null && counterErrorId != null) {
+        var hideCount = 0;
+        var updateCount = setInterval(function() {
+            var now = new Date().getTime();
+            var distance = countDownDate - now;
+
+            // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            counterErrorId.innerHTML = (hours <= 9 ? "0" + hours : hours) + ":"
+                         + (minutes <= 9 ? "0" + minutes : minutes) + ":"
+                         + (seconds <= 9 ? "0" + seconds : seconds);
+
+            hideCount++;
+            if (hideCount > 10 || distance < 0) {
+                clearInterval(updateCount);
+                counterErrorId.innerHTML = "";
+                counterErrorId.style.display = "none";
+                exceptionErrorId.innerHTML = "";
+                exceptionErrorId.style.display = "none";
+            }
+        }, 1000);
+    } else if (exceptionErrorId.innerHTML == "* E-mail or Password are incorrect" || exceptionErrorId.innerHTML == "* E-mail sau Parola sunt greșite") {
+        var hideCount = 0;
+        var updateCount = setInterval(function() {
+            hideCount++;
+            if (hideCount > 10) {
+                clearInterval(updateCount);
+                exceptionErrorId.innerHTML = "";
+                exceptionErrorId.style.display = "none";
+            }
+        }, 1000);
     }
 }
