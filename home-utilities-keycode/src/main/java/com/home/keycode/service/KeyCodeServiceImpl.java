77d3550c2dcac20f6a806521853aeebbf4a093a6
Andrei <andrei_brainiac@yahoo.com>
2022-04-25 15:35:59 +0300

    Update KeyCodeServiceImpl.java

--- home-utilities-keycode/src/main/java/com/home/keycode/service/KeyCodeServiceImpl.java
+++ home-utilities-keycode/src/main/java/com/home/keycode/service/KeyCodeServiceImpl.java
@@ -136,7 +136,7 @@ public class KeyCodeServiceImpl implements KeyCodeService {
         final var path = sanitizePath(new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().getFile().replace(PATH_TO_REPLACE, GENERATED_KEYCODE_PATH.concat(GENERATED_KEYCODE_FILE_NAME))).getParent());
 */
         final var path = new ClassPathResource(sanitizePath(new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().getFile().replace(PATH_TO_REPLACE, GENERATED_KEYCODE_PATH)).getParent()));
-        final var file = new File(path.getPath() + "keycode1.jpg");
+        final var file = new File(path.getPath() + "/keycode1.jpg");
         System.out.println("Path: -> " + file.getPath());
         //final var file = new File(path);
         final var createdStatus = file.createNewFile();
